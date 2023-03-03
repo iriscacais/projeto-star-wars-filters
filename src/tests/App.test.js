8a2ecd9e55f07c4d6ed94751c11a5c4c212b49bc
Iris Cacais <112999212+iriscacais@users.noreply.github.com>
2023-03-03 12:57:32 -0300

    requisito 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,46 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, renderHook, screen, waitFor } from '@testing-library/react';
 import App from '../App';
+import mock from './mockData'
+import userEvent from '@testing-library/user-event';
 
-test('I am your test', () => {
+
+test('se é renderizado na tela um input do tipo texto com datatest ', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputText= screen.getByTestId('name-filter');
+  expect(inputText).toBeInTheDocument();
+
 });
+test('se os inputs da filtragem numerica estao na tela', () => {
+  render(<App />); 
+  const inputColumn= screen.getByTestId('column-filter');
+  expect(inputColumn).toBeInTheDocument();
+
+  const inputOperador= screen.getByTestId('comparison-filter');
+  expect(inputOperador).toBeInTheDocument();
+
+  const inputNumber= screen.getByTestId('value-filter');
+  expect(inputNumber).toBeInTheDocument();
+})
+  
+
+test('se é possível digitar nos inputs corretamente', async () => {
+  render(<App />)
+  const inputText= screen.getByTestId('name-filter');
+  userEvent.type(inputText, 'oo')
+
+  const inputColumn= screen.getByTestId('column-filter');
+  userEvent.selectOptions(inputColumn, 'population')
+
+  const inputOperador= screen.getByTestId('comparison-filter');
+  userEvent.selectOptions(inputOperador, 'igual a')
+
+  const inputNumber= screen.getByTestId('value-filter');
+  userEvent.type(inputNumber, '1000');
+
+  const filterButton = screen.getByTestId('button-filter')
+  userEvent.click(filterButton)
+
+})
+
+
